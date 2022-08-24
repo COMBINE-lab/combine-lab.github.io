7ca862401dfc5fbf35594a3c81fd5d9d9ffd6734
Rob Patro <rob-p@users.noreply.github.com>
2022-08-23 21:27:00 -0400

    Create yuan-gao.m

---
+++ _members/yuan-gao.m
@@ -0,0 +1,15 @@
+---
+title: Yuan Gao
+search:
+  - Y. Gao
+  - Yuan
+role: phd
+group: current
+image: images/yuan.jpg
+email: ygao61@umd.edu
+github: ygao61
+---
+
+Yuan Gao is a PhD student in the BISI program. She obtained her bachelor's degree in biological science at Liaoning University in China, 
+and her MS in Bioinformatics at Georgetown university. She is interested in developing computational methods to analyze, compare, 
+and organize single-cell RNA sequencing data. 
